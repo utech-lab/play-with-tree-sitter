f6f297963f80141fdce772bfb086924ede37fcfc
utech-lab <75456674+utech-lab@users.noreply.github.com>
2026-08-02 10:34:21 +0900

    Add files via upload

---
+++ tree-sitter-scm/css_tags.scm
@@ -0,0 +1,13 @@
+(id_selector
+  (id_name) @name) @definition.interface
+
+(class_selector
+  (class_name) @name) @definition.class
+
+(tag_name) @name @definition.type
+
+;;(declaration
+;;  (property_name) @name) @definition.variable
+
+(media_statement
+  (_) @name) @definition.important
